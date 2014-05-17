33156e0eee3862eba32fb622f3ab5a500a22bbaf
Peggy Griffin <seishingithub@gmail.com>
2014-05-17 14:44:22 -0600

    User can login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,15 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by_username(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:id] = user.id
+      redirect_to login_path, notice: "Thanks for logging in!
+                                        You are logged in as #{user.username}!"
+    else
+      redirect_to login_path, notice: 'Something went wrong'
+    end
+  end
 end
